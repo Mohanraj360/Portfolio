9ef7498d0300a0a3ef28e4dfa63cb259beb279e6
S.MOHANRAJ <94728754+Mohanraj360@users.noreply.github.com>
2024-06-08 14:59:13 +0530

    content.ts

--- modules/home/home-resume/content.ts
+++ modules/home/home-resume/content.ts
@@ -1,9 +1,9 @@
 export const resumeHeading = "My Resume";
 export const developerName = "S.Mohanraj";
-export const developerPosition = "Final Year Student";
+export const developerPosition = "CS Student";
 export const developerStudies = "MCA";
 export const developerClg = "VIT, Vellore";
 export const developerDescription =
-  "I am S.Mohanraj , a diligent frontend developer currently in my Final year. I'm currently pursuing Master of Computer Applications.";
+  "I am S.Mohanraj , a diligent frontend developer. I'm graduate with Master of Computer Applications.";
 export const resumeCtaText = "View Resume";
 export const resumeLink = "https://drive.google.com/file/d/1BG4vQh-SH__exizN-boA93PMGhOFL_NI/view?usp=sharing";
