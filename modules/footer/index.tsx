d0b72afe549f93c4e4c593df76fb14b4da5d1cf3
S.MOHANRAJ <94728754+Mohanraj360@users.noreply.github.com>
2024-06-08 15:02:57 +0530

    index.tsx

--- modules/footer/index.tsx
+++ modules/footer/index.tsx
@@ -1,7 +1,6 @@
 import NoiseTexture from "@/components/noise-texture";
 import FooterBg from "@/modules/footer/footer-bg";
 import FooterCopyrights from "@/modules/footer/footer-copyrights";
-import FooterCredits from "@/modules/footer/footer-credits";
 import FooterMain from "@/modules/footer/footer-main";
 import { footerBlobOneCss, footerWrapperCss } from "@/modules/footer/styles";
 
