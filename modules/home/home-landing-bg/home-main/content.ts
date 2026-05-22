b692f035a4e6b1cf8e781d818fa49c976f3e49f3
S.MOHANRAJ <94728754+Mohanraj360@users.noreply.github.com>
2026-05-23 00:35:38 +0530

    Update content.ts

--- modules/home/home-landing-bg/home-main/content.ts
+++ modules/home/home-landing-bg/home-main/content.ts
@@ -1,3 +1,3 @@
-export const h1Content = ["I'm S.Mohanraj", "Data Analyst", "Web Developer."];
+export const h1Contents = ["I'm S.Mohanraj", "Data Analyst", "Web Developer."];
 export const h2Text =
   "I bridge the gap between deep data analytics and high-performance web development, crafting accessible and visually captivating digital solutions.";
