e2be714ad1c74dfa986899d7e5bc10296bea7277
S.MOHANRAJ <94728754+Mohanraj360@users.noreply.github.com>
2026-05-23 00:12:38 +0530

    Update content.ts

--- modules/home/home-about/content.ts
+++ modules/home/home-about/content.ts
@@ -1,4 +1,4 @@
 export const aboutHeadingText = "About Me";
 export const aboutText =
-  "Data Analyst specializing in retail insights. I’m passionate about building AI-driven automation and intelligent business solutions that turn complex data into actionable strategy. Welcome to my portfolio!. I'm open to freelancing opportunities and eager to collaborate!.";
+  "Data Analyst specializing in retail insights. I’m passionate about building AI-driven automation and intelligent business solutions that turn complex data into actionable strategy. Welcome to my portfolio. I'm open to freelancing opportunities and eager to collaborate.";
 export const aboutCtaText = "Contact";
